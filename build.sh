6bce1c6710fdaca35778028e53df7472faf38c21
Zedr0n <zedr0nre@gmail.com>
2009-10-04 02:43:25 +0400

    moving binaries as part of build script

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
+echo "-------------------"
 echo "Building plugins..."
+echo "-------------------"
 echo "Building CDVDiso..."
 pushd ./plugins/CDVDiso/Linux
-make
+make 
 popd
+mv ./plugins/CDVDiso/Linux/libCDVDiso.so ./bin
 echo "Building CDVDnull..."
 pushd ./plugins/CDVDnull
 make
 popd
+mv ./plugins/CDVDnull/libCDVDnull.so ./bin
+echo "--------------"
 echo "Building pcsx2"
+echo "--------------"
 aclocal
 autoconf
 automake
 ./configure --enable-devbuild --enable-sse3
 make
+mv ./Linux/pcsx2 ./bin
