cf51fd787bc73fb0e165b2492c541ec20e15262a
Zedr0n <zedr0nre@gmail.com>
2009-10-04 03:34:01 +0400

    adding bin folder

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ echo "Building CDVDiso..."
 pushd ./plugins/CDVDiso/Linux
 make
 popd
-mv ./plugins/CDVDiso/Linux/libCDVDiso.so ./bin/
-mv ./plugins/CDVDiso/Linux/cfgCDVDiso ./bin/
+mv ./plugins/CDVDiso/Linux/libCDVDiso.so ./bin/plugins
+mv ./plugins/CDVDiso/Linux/cfgCDVDiso ./bin/plugins
 echo "Building CDVDnull..."
 pushd ./plugins/CDVDnull
 make
 popd
-mv ./plugins/CDVDnull/libCDVDnull.so ./bin/
+mv ./plugins/CDVDnull/libCDVDnull.so ./bin/plugins
 echo "Building zeropad"
 pushd ./plugins/zeropad
 aclocal
@@ -23,12 +23,12 @@ chmod +x ./configure
 ./configure
 make
 popd
-mv ./plugins/zeropad/libZeroPAD.so.0.2.0 ./bin/libZeroPad.so
+mv ./plugins/zeropad/libZeroPAD.so.0.2.0 ./bin/plugins/libZeroPad.so
 echo "Building Peopsspu2"
 pushd ./plugins/PeopsSPU2_SDL
 make
 popd
-mv ./plugins/PeopsSPU2_SDL/libspu2PeopsSDL.so ./bin/
+mv ./plugins/PeopsSPU2_SDL/libspu2PeopsSDL.so ./bin/plugins
 echo "Building spu2null"
 pushd ./plugins/SPU2null
 aclocal
@@ -38,10 +38,10 @@ chmod +x ./configure
 ./configure
 make
 popd
-mv ./plugins/spu2null/libSPU2null.so.0.7.1 ./bin/libSPU2null.so
-mv ./plugins/cfg* ./bin
+mv ./plugins/spu2null/libSPU2null.so.0.7.1 ./bin/plugins/libSPU2null.so
+mv ./plugins/cfg* ./bin/plugins
 chmod +x ./bin/*
-mv ./plugins/lib* ./bin
+mv ./plugins/lib* ./bin/plugins
 echo "--------------"
 echo "Building pcsx2"
 echo "--------------"
