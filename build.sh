dff2829f7aa64fdb25a22c89526dfec9e7cfc625
Zedr0n <zedr0nre@gmail.com>
2009-10-04 02:34:35 +0400

    build script updated to include cdvd plugins

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+echo "Building plugins..."
+echo "Building CDVDiso..."
+pushd ./plugins/CDVDiso/Linux
+make
+popd
+echo "Building CDVDnull..."
+pushd ./plugins/CDVDnull
+make
+popd
+echo "Building pcsx2"
 aclocal
 autoconf
 automake
