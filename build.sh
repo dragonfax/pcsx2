2ea819536d1df6f21bf37b8059d3f295eab0c08e
Zedr0n <zedr0nre@gmail.com>
2009-10-17 15:55:22 +0100

    updating build system
    moving building to ./build subdirectories and
    cleaning up build messages

--- build.sh
+++ build.sh
@@ -4,99 +4,136 @@ export PKG_CONFIG_PATH=/usr/X11/lib/pkgconfig
 if [ $1 == "--enable-gdb" ]; then
 	GDB="--enable-gdb"
 fi
-pushd bin
-mkdir bios
-mkdir frames
-mkdir help
-mkdir logs
-mkdir sstates
-popd
+pushd bin >/dev/null
+mkdir bios &>/dev/null
+mkdir frames &>/dev/null
+mkdir help &>/dev/null
+mkdir logs &>/dev/null
+mkdir sstates &>/dev/null
+popd >/dev/null
 echo "-------------------"
 echo "Building plugins..."
 echo "-------------------"
 echo "Building CDVDnull..."
-pushd ./plugins/CDVDnull
+pushd ./plugins/CDVDnull >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/CDVDnull/libCDVDnull.so.0.8.0 ./bin/plugins/libCDVDnull.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/CDVDnull/build/libCDVDnull.so.0.8.0 ./bin/plugins/libCDVDnull.so
 echo "Building CDVDiso..."
-pushd ./plugins/CDVDiso/src
+pushd ./plugins/CDVDiso/src >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+cd ..
+mkdir build &>/dev/null
+pushd build >/dev/null
+../src/configure $GDB
 make
-popd
-mv ./plugins/CDVDiso/src/libCDVDiso.so.0.9.0 ./bin/plugins/libCDVDiso.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/CDVDiso/build/libCDVDiso.so.0.9.0 ./bin/plugins/libCDVDiso.so
 echo "Building DEV9null..."
-pushd ./plugins/dev9null
+pushd ./plugins/dev9null >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/dev9null/libDEV9null.so.0.4.0 ./bin/plugins/libDEV9null.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/dev9null/build/libDEV9null.so.0.4.0 ./bin/plugins/libDEV9null.so
 echo "Building FWnull..."
-pushd ./plugins/FWnull
+pushd ./plugins/FWnull >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/FWnull/libFWnull.so.0.5.0 ./bin/plugins/libFWnull.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/FWnull/build/libFWnull.so.0.5.0 ./bin/plugins/libFWnull.so
 echo "Building SPU2null..."
 pushd ./plugins/SPU2null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/SPU2null/libSPU2null.so.0.7.1 ./bin/plugins/libSPU2null.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/SPU2null/build/libSPU2null.so.0.7.1 ./bin/plugins/libSPU2null.so
 echo "Building USBnull..."
-pushd ./plugins/USBnull
+pushd ./plugins/USBnull >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/USBnull/libUSBnull.so.0.6.0 ./bin/plugins/libUSBnull.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/USBnull/build/libUSBnull.so.0.6.0 ./bin/plugins/libUSBnull.so
 echo "Building zeropad..."
-pushd ./plugins/zeropad
+pushd ./plugins/zeropad >/dev/null
 aclocal
 autoconf
 automake --add-missing
-./configure $GDB
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure $GDB
 make
-popd
-mv ./plugins/zeropad/libZeroPad.so.0.3.0 ./bin/plugins/libZeroPad.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/zeropad/build/libZeroPad.so.0.3.0 ./bin/plugins/libZeroPad.so
 echo "Building zzogl 0.17.156"
-pushd ./plugins/zzogl
+pushd ./plugins/zzogl >/dev/null
 aclocal
 autoconf
-automake
-./configure --enable-devbuild --enable-sse2 $GDB
+automake --add-missing
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure --enable-devbuild --enable-sse2 $GDB
 make
-popd
-mv ./plugins/zzogl/libZeroGSogl.so.* ./bin/plugins/libZeroGSogl.so
+popd >/dev/null
+popd >/dev/null
+mv ./plugins/zzogl/build/libZeroGSogl.so.* ./bin/plugins/libZeroGSogl.so
 cp ./plugins/zzogl/ps2hw.dat ./bin/plugins
 
 echo "--------------"
 echo "Building pcsx2"
 echo "--------------"
-pushd ./pcsx2
+pushd ./pcsx2 >/dev/null
 aclocal
 autoconf
-automake
-./configure --enable-devbuild --enable-sse3 $GDB
+automake --add-missing
+rm -rf autom4te.cache
+mkdir build &>/dev/null
+pushd build >/dev/null
+../configure --enable-devbuild --enable-sse3 $GDB
 make
-mv ./Linux/pcsx2 ../bin/
-popd
+popd >/dev/null
+mv ./build/Linux/pcsx2 ../bin/
+popd >/dev/null
 chmod +x ./bin/*
